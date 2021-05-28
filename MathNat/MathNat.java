219dcc522e4e80c831419c450eba63c18673e7b8
m05769js <jack.strange@student.manchester.ac.uk>
2021-05-28 13:15:28 +0100

    nat: readding P, sub

--- MathNat/MathNat.java
+++ MathNat/MathNat.java
@@ -9,6 +9,11 @@ public class MathNat{
 		return new MathNat(n);
 	}
 
+	public static MathNat P(MathNat n){
+		if(n == null) return null;
+		else return n.next;
+	}
+
 	public static MathNat init(int n){
 		if(n == 0) return null;
 		else return S(init(n-1));
@@ -16,28 +21,27 @@ public class MathNat{
 
 	public static int realVal(MathNat n){
 		if(n == null) return 0;
-		else return 1 + realVal(n.next);
+		else return 1 + realVal(P(n));
 	}
 
 	public static String toString(MathNat n){
 		if(n == null) return "0";
-		else return "S" + toString(n.next);
+		else return "S" + toString(P(n));
 	}
 
 	public static MathNat plus(MathNat n1, MathNat n2){
 		if(n2 == null) return n1;
-		else return S(plus(n1,n2.next));
+		else return S(plus(n1,P(n2)));
 	}
 
 	public static MathNat prod(MathNat n1, MathNat n2){
 		if(n2 == null) return null;
-		else return plus(prod(n1,n2.next),n1);
+		else return plus(prod(n1,P(n2)),n1);
 	}
 
 	public static MathNat sub(MathNat n1, MathNat n2){
-		System.out.println(toString(n1) + " - " + toString(n2));
-		if(n2 == null) return null;
-		else return sub(n1,n2.next).next;
+		if(n2 == null) return n1;
+		else return P(sub(n1,P(n2)));
 	}
 
 	public static void main(String[] args){
@@ -46,9 +50,9 @@ public class MathNat{
 		System.out.println("4 = " + toString(nat4) + " = " + Integer.toString(realVal(nat4)));
 		System.out.println("6 = " + toString(nat6) + " = " + Integer.toString(realVal(nat6)));
 		System.out.println();
-		System.out.println("Plus: " + toString(plus(nat4,nat6)) + " = " + Integer.toString(realVal(plus(nat4,nat6))));
-		System.out.println("Prod: " + toString(prod(nat4,nat6)) + " = " + Integer.toString(realVal(prod(nat4,nat6))));
-		System.out.println("Sub1: " + toString(sub(nat4,nat6)) + " = " + Integer.toString(realVal(sub(nat4,nat6))));
-		System.out.println("Sub2: " + toString(sub(nat6,nat4)) + " = " + Integer.toString(realVal(sub(nat6,nat4))));
+		System.out.println("4+6 = " + toString(plus(nat4,nat6)) + " = " + Integer.toString(realVal(plus(nat4,nat6))));
+		System.out.println("4*6 = " + toString(prod(nat4,nat6)) + " = " + Integer.toString(realVal(prod(nat4,nat6))));
+		System.out.println("6-4 = " + toString(sub(nat6,nat4)) + " = " + Integer.toString(realVal(sub(nat6,nat4))));
+		System.out.println("4-6 = " + toString(sub(nat4,nat6)) + " = " + Integer.toString(realVal(sub(nat4,nat6))));
 	}
 }
