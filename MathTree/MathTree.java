119ae5e6445e3c8765cb4c840a7841c8a4105b2a
m05769js <jack.strange@student.manchester.ac.uk>
2021-05-28 12:01:12 +0100

    Setting Up MathTree

--- MathTree/MathTree.java
+++ MathTree/MathTree.java
@@ -1,1 +1,37 @@
-public class
+public class MathTree{
+	public int value;
+	public MathTree left,right;
+
+	public MathTree(int v, MathTree l, MathTree r){
+		this.value = v;
+		this.left = l;
+		this.right = r;
+	}
+
+	public static MathTree init(int v, MathTree l, MathTree r){
+		return new MathTree(v,l,r);
+	}
+
+	public static MathTree init(int v){
+		return new MathTree(v,null,null);
+	}
+
+	public static MathTree init(){
+		return null;
+	}
+
+	public static String toString(MathTree t){
+		if(t == null){
+			return "-";
+		}else if(t.left == null && t.right == null){
+			return Integer.toString(t.value);
+		}else{
+			return Integer.toString(t.value) + ":(" + toString(t.left) + ", " + toString(t.right) + ")";
+		}
+	}
+
+	public static void main(String[] args){
+		MathTree tree = init(3,init(6),init(5,init(4),init(8,init(),init(7))));
+		System.out.println(toString(tree));
+	}
+}
