d9d9d4e71730ae3e6733cd50b09122cb62443e7b
m05769js <jack.strange@student.manchester.ac.uk>
2021-07-13 13:42:51 +0100

    Ant Sim File

--- RegLanguages/LanguageBase.java
+++ RegLanguages/LanguageBase.java
@@ -1,61 +1,78 @@
 import java.util.ArrayList;
 public class LanguageBase{
 	
-	public class Regex{
-		public String regex;
+}
+class Regex{
+	public String regex;
 
-		public static NFAE toNFAE(Regex regex){
+	public static NFAE toNFAE(Regex regex){
+		return null;
+	}
+	public static NFA toNFA(Regex regex){
+		return NFAE.toNFA(toNFAE(regex));
+	}
+	public static DFA toDFA(Regex regex){
+		return NFA.toDFA(toNFA(regex));
+	}
+}
+class TransFunc{
+	public Integer start;
+	public Character symbol;
+	public Integer end;
+}
+abstract class Automaton{
+	public ArrayList<Integer> states;
+	public Integer start;
+	public ArrayList<Integer> accepting;
+	public ArrayList<TransFunc> delta;
+}
+class DFA extends Automaton{
+	public static Regex toRegex(DFA dfa){
+		return null;
+	}
+	public static NFAE toNFAE(DFA dfa){
+		return Regex.toNFAE(toRegex(dfa));
+	}
+	public static NFA toNFA(DFA dfa){
+		return NFAE.toNFA(toNFAE(dfa));
+	}
+}
+class NFA extends Automaton{
+	public static DFA toDFA(NFA nfa){
+		return null;
+	}
+	public static Regex toRegex(NFA nfa){
+		return DFA.toRegex(toDFA(nfa));
+	}
+	public static NFAE toNFAE(NFA nfa){
+		return Regex.toNFAE(toRegex(nfa));
+	}
+}
+class NFAE extends Automaton{
+	public static NFA toNFA(NFAE nfae){
+		NFA nfa = new NFA();
+		for(Integer state:nfae.states) nfa.states.add(state);
+		nfa.start = nfae.start;
+		for(Integer state:nfae.accepting) nfa.accepting.add(state);
+		for(TransFunc function:nfae.delta) nfa.delta.add(function);
+		boolean valid = false;
+		while(!valid){
+			valid = true;
+			for(TransFunc function:nfa.delta){
+				if(function.symbol == "epsilon"){
+					valid = false;
+					if(!nfa.accepting.contains(function.start)) nfa.accepting.add(function.start);
+					nfa.delta.remove(function);
+				}
+			}
+		}
 
-		}
-		public static NFA toNFA(Regex regex){
-			NFAE.toNFA(toNFAE(regex));
-		}
-		public static DFA toDFA(Regex regex){
-			NFA.toDFA(toNFA(regex));
-		}
+		return nfa;
 	}
-	public class TransFunc{
-		public Integer start;
-		public Character symbol;
-		public Integer end;
+	public static DFA toDFA(NFAE nfae){
+		return NFA.toDFA(toNFA(nfae));
 	}
-	public abstract class Automaton{
-		public ArrayList<Integer> states;
-		public Integer start;
-		public ArrayList<Integer> accepting;
-		public ArrayList<TransFunc> delta;
-	}
-	public class DFA extends Automaton{
-		public static Regex toRegex(DFA dfa){
-			
-		}
-		public static NFAE toNFAE(DFA dfa){
-			Regex.toNFAE(toRegex(dfa));
-		}
-		public static NFA toNFA(DFA dfa){
-			NFAE.toNFA(toNFAE(dfa));
-		}
-	}
-	public class NFA extends Automaton{
-		public static DFA toDFA(NFA nfa){
-
-		}
-		public static Regex toRegex(NFA nfa){
-			DFA.toRegex(toDFA(nfa));
-		}
-		public static NFAE toNFAE(NFA nfa){
-			Regex.toNFAE(toRegex(nfa));
-		}
-	}
-	public class NFAE extends Automaton{
-		public static NFA toNFA(NFAE nfae){
-
-		}
-		public static DFA toDFA(NFAE nfae){
-			NFA.toDFA(toNFA(nfae));
-		}
-		public static Regex toRegex(NFAE nfae){
-			DFA.toRegex(toDFA(nfae));
-		}
+	public static Regex toRegex(NFAE nfae){
+		return DFA.toRegex(toDFA(nfae));
 	}
 }
